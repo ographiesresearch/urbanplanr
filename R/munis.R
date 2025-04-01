71c0acba6dd7c29d6fe6bae1a81f818e322f9886
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2025-04-01 11:50:58 -0400

    fix `muni_` documentation

--- R/munis.R
+++ R/munis.R
@@ -1,8 +1,14 @@
-munis_preprocess <- function(df) {
+#' Preprocessing Workflow for Municipalities
+#'
+#' @param df An `sf` object.
+#' @param crs Target coordinate reference system.
+#'
+#' @returns An `sf` object.
+#' @export
+munis_preprocess <- function(df, crs) {
   df |>
-    # TODO: Replace with `st_geom_to_xy()`.
-    # center_xy() |>
-    sf_preprocess() |>
+    st_geom_to_xy() |>
+    sf_preprocess(crs) |>
     dplyr::select(
       pl_name,
       state,
@@ -18,9 +24,43 @@ munis_preprocess <- function(df) {
     )
 }
 
-munis_get_ct <- function(crs) {
+#' Downloads Municipal Boundaries by State
+#' @name muni_get_state
+#' 
+#' @description
+#' `muni_get_ma()` Downloads municipal boundaries for Massachusetts.
+#' 
+#' `muni_get_ct()` Downloads municipal boundaries for Connecticut.
+#' 
+#' `muni_get_me()` Downloads municipal boundaries for Maine.
+#' 
+#' `muni_get_nh()` Downloads municipal boundaries for New Hampshire.
+#' 
+#' `muni_get_nj()` Downloads municipal boundaries for New Jersey.
+#' 
+#' `muni_get_ny()` Downloads municipal boundaries for New York.
+#' 
+#' `muni_get_ri()` Downloads municipal boundaries for Rhode Island.
+#' 
+#' `muni_get_vt()` Downloads municipal boundaries for Vermont.
+#'
+#' @returns An `sf` object.
+#' @export
+munis_get_ma <- function() {
+  message("Downloading Massachusetts municipal boundaries...")
+  utils_get_arc("43664de869ca4b06a322c429473c65e5_0") |>
+    dplyr::mutate(
+      town = stringr::str_to_title(town),
+      state = "MA"
+    ) |>
+    dplyr::select(pl_name = town, state)
+}
+
+#' @name muni_get_state
+#' @export
+munis_get_ct <- function() {
   message("Downloading Connecticut municipal boundaries...")
-  get_from_arc("df1f6d681b7e41dca8bdd03fc9ae0dd6_1", crs = crs) |>
+  utils_get_arc("df1f6d681b7e41dca8bdd03fc9ae0dd6_1") |>
     dplyr::filter(
       town != " ", town != ""
     ) |>
@@ -34,19 +74,11 @@ munis_get_ct <- function(crs) {
     dplyr::ungroup()
 }
 
-munis_get_ma <- function(crs) {
-  message("Downloading Massachusetts municipal boundaries...")
-  get_from_arc("43664de869ca4b06a322c429473c65e5_0", crs = crs) |>
-    dplyr::mutate(
-      town = stringr::str_to_title(town),
-      state = "MA"
-    ) |>
-    dplyr::select(pl_name = town, state)
-}
-
-munis_get_me <- function(crs) {
+#' @name muni_get_state
+#' @export
+munis_get_me <- function() {
   message("Downloading Maine municipal boundaries...")
-  get_from_arc("289a91e826fd4f518debdd824d5dd16d_0", crs = crs) |>
+  utils_get_arc("289a91e826fd4f518debdd824d5dd16d_0") |>
     dplyr::filter(
       town != " "
     ) |>
@@ -60,9 +92,11 @@ munis_get_me <- function(crs) {
     )
 }
 
-munis_get_nh <- function(crs) {
+#' @name muni_get_state
+#' @export
+munis_get_nh <- function() {
   message("Downloading New Hampshire municipal boundaries...")
-  get_from_arc("4edf75ab263b4d92996f92fb9cf435fa_8", crs = crs) |>
+  utils_get_arc("4edf75ab263b4d92996f92fb9cf435fa_8") |>
     dplyr::filter(
       pbpname != " "
     ) |>
@@ -72,7 +106,9 @@ munis_get_nh <- function(crs) {
     dplyr::select(pl_name = pbpname, state)
 }
 
-munis_get_nj <- function(crs) {
+#' @name muni_get_state
+#' @export
+munis_get_nj <- function() {
   url <- httr::parse_url("https://services2.arcgis.com/XVOqAjTOJ5P6ngMu/ArcGIS/rest/services")
   url$path <- paste(url$path, "NJ_Municipal_Boundaries_3424/FeatureServer/0/query", sep = "/")
   url$query <- list(where = "POPDEN2020>0",
@@ -83,19 +119,18 @@ munis_get_nj <- function(crs) {
   
   sf::st_read(request) |>
     dplyr::rename_with(tolower) |>
-    sf::st_transform(crs) |>
     dplyr::select(
       name
     ) |>
     dplyr::mutate(state = "NJ")
 }
 
-
-munis_get_ny <- function(crs) {
-  get_shp_from_remote_zip(
+#' @name muni_get_state
+#' @export
+munis_get_ny <- function() {
+  utils_get_remote_shp(
     "https://gisdata.ny.gov/GISData/State/Civil_Boundaries/NYS_Civil_Boundaries.shp.zip",
-    shpfile="Cities_Towns.shp",
-    crs=crs
+    layer="Cities_Towns.shp"
   ) |>
     dplyr::select(
       name
@@ -103,9 +138,11 @@ munis_get_ny <- function(crs) {
     dplyr::mutate(state = "NY")
 }
 
-munis_get_ri <- function(crs) {
+#' @name muni_get_state
+#' @export
+munis_get_ri <- function() {
   message("Downloading Rhode Island municipal boundaries...")
-  get_from_arc("957468e8bb3245e8b3321a7bf3b6d4aa_0", crs = crs) |>
+  utils_get_arc("957468e8bb3245e8b3321a7bf3b6d4aa_0") |>
     dplyr::filter(
       name != " ", name != ""
     ) |>
@@ -123,9 +160,11 @@ munis_get_ri <- function(crs) {
     )
 }
 
-munis_get_vt <- function(crs) {
+#' @name muni_get_state
+#' @export
+munis_get_vt <- function() {
   message("Downloading Vermont municipal boundaries...")
-  get_from_arc("3f464b0e1980450e9026430a635bff0a_0", crs = crs) |>
+  utils_get_arc("3f464b0e1980450e9026430a635bff0a_0") |>
     dplyr::filter(
       townnamemc != " "
     ) |>
@@ -135,75 +174,62 @@ munis_get_vt <- function(crs) {
     dplyr::select(pl_name = townnamemc, state)
 }
 
-munis_process <- function(df) {
-  df |>
-    dplyr::mutate(
-      city = stringr::str_detect(
-        NAME, "city,"
-      )
-    )
-}
-
-munis_select <- function(place_geo, places) {
-  searches <- dplyr::bind_rows(places) |>
-    dplyr::mutate(
-      pl_id = stringr::str_c(
-        stringr::str_to_lower(place), 
-        stringr::str_to_lower(state), 
-        sep="_"
-      ),
-      pl_id = stringr::str_c("^", pl_id, "$", sep="")
-    ) |>
-    dplyr::pull(pl_id) |>
-    stringr::str_c(collapse="|")
-  
-  places_vector <- dplyr::bind_rows(places) |>
-    dplyr::pull(place)
-  
-  matched <- place_geo |>
-    dplyr::mutate(
-      selected = stringr::str_detect(pl_id, searches)
-    )
-  match_count <- nrow(matched |> dplyr::filter(selected))
-  if (match_count == length(places)) {
-    message(glue::glue("Exact match found for all place names ({stringr::str_c(places_vector, collapse=', ')})."))
-  } else if (match_count > length(places)) {
-    message(glue::glue("Ambiguous place names provided."))
-    stop()
-  } else {
-    message(glue::glue("Unable to match all place names."))
-    stop()
-  }
-  matched
-}
-
-muni_decision <- function(states, crs) {
+#' Execute Municipality-Getter Using State Name
+#'
+#' @param states Character vector of state names.
+#' @param crs target coordinate reference system.
+#'
+#' @returns An `sf` object
+#' @export
+munis_decision <- function(states, crs) {
   state_munis <- list()
   no_muni_st <- c()
-  for (state in states) {
-    if (state == "CT") {
-      state_munis[[state]] <- munis_get_ct(crs) 
-    } else if (state == "MA") {
-      state_munis[[state]] <- munis_get_ma(crs)
-    } else if (state == "ME") {
-      state_munis[[state]] <- munis_get_me(crs)
-    } else if (state == "NH") {
-      state_munis[[state]] <- munis_get_nh(crs)
-    } else if (state == "NJ") {
-      state_munis[[state]] <- munis_get_nj(crs)
-    } else if (state == "NY") {
-      state_munis[[state]] <- munis_get_ny(crs)
-    } else if (state == "RI") {
-      state_munis[[state]] <- munis_get_ri(crs)
-    } else if (state == "VT") {
-      state_munis[[state]] <- munis_get_vt(crs)
+  states <- stringr::str_to_lower(states)
+  for (s in states) {
+    func <- glue::glue("munis_get_{s}")
+    if (exists(func) && is.function(func)) {
+      state_munis[[s]] <- do.call(func, args=list())
     } else {
-      no_muni_st <- append(no_muni_st, state)
+      message(glue::glue("No source for municipalities defined via {func}."))
+      no_muni_st <- append(no_muni_st, s)
     }
   }
   if (length(no_muni_st) > 0) {
-    state_munis[["Other"]] <- get_places(states = no_muni_st, crs = crs)
+    state_munis[["Other"]] <- tigris_get_places(states = no_muni_st)
   }
   dplyr::bind_rows(state_munis) |>
     munis_preprocess(crs)
 }
+
+# munis_select <- function(place_geo, places) {
+#   searches <- dplyr::bind_rows(places) |>
+#     dplyr::mutate(
+#       pl_id = stringr::str_c(
+#         stringr::str_to_lower(place), 
+#         stringr::str_to_lower(state), 
+#         sep="_"
+#       ),
+#       pl_id = stringr::str_c("^", pl_id, "$", sep="")
+#     ) |>
+#     dplyr::pull(pl_id) |>
+#     stringr::str_c(collapse="|")
+#   
+#   places_vector <- dplyr::bind_rows(places) |>
+#     dplyr::pull(place)
+#   
+#   matched <- place_geo |>
+#     dplyr::mutate(
+#       selected = stringr::str_detect(pl_id, searches)
+#     )
+#   match_count <- nrow(matched |> dplyr::filter(selected))
+#   if (match_count == length(places)) {
+#     message(glue::glue("Exact match found for all place names ({stringr::str_c(places_vector, collapse=', ')})."))
+#   } else if (match_count > length(places)) {
+#     message(glue::glue("Ambiguous place names provided."))
+#     stop()
+#   } else {
+#     message(glue::glue("Unable to match all place names."))
+#     stop()
+#   }
+#   matched
+# }
