ba12d5c749617ca260cb988608316edf2c525733
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2025-04-02 18:01:00 -0400

    revise `gtfs.R` functions

--- R/gtfs.R
+++ R/gtfs.R
@@ -1,63 +1,74 @@
+#' GTFS Helper Functions
+#' @name gtfs_helpers
+#' @description
+#' `gtfs_routes()` takes a `tidygtfs` object and returns routes as `sf`.
+#' `gtfs_stops()` takes a `tidygtfs` object and returns stops as `sf`.
+#' `gtfs_process()` removes stops with no lat-longs and builds `sf` objects.
+#' 
+#' @param gtfs A `tidygtfs` object.
+#' @param crs `crs` object or EPSG code.
+#'
+#' @returns An `sf` object
+#' @export
+gtfs_routes <- function(gtfs, crs) {
+  routes <- gtfs |>
+    tidytransit::get_route_geometry() |>
+    dplyr::left_join(
+      gtfs$routes,
+      by=dplyr::join_by(route_id)
+    ) |>
+    sf::st_transform(crs)
+}
+
+#' @name gtfs_helpers
+#' @export
+gtfs_stops <- function(gtfs, crs) {
+  stops <- gtfs$stops |>
+    tidytransit::stops_as_sf() |>
+    sf::st_transform(crs)
+}
+
+#' @name gtfs_helpers
+#' @export
 gtfs_process <- function(gtfs, crs) {
   gtfs <- gtfs |>
     tidytransit::empty_strings_to_na()
   
-  gtfs$stops <- gtfs$stops  |> 
+  gtfs$stops <- gtfs$stops |> 
     tidyr::drop_na(stop_lat, stop_lon)
   
-  routes <- gtfs |>
-    tidytransit::gtfs_as_sf() |>
-    tidytransit::get_route_geometry(
-      route_ids = gtfs$routes$route_id
-    ) |>
-    dplyr::left_join(
-      gtfs$routes |>
-        dplyr::select(
-          route_id,
-          desc = route_desc,
-          name = route_long_name,
-          short_name = route_short_name
-        ),
-      by=dplyr::join_by(route_id)
-    ) |>
-    sf::st_transform(crs)
+  gtfs <- gtfs |>
+    tidytransit::gtfs_as_sf()
   
-  gtfs$stops <- gtfs |>
-    tidytransit::filter_stops(
-      service_ids=gtfs$calendar$service_id,
-      route_ids=route_ids
-    )
-  
-  stops <- gtfs$stops |>
-    tidytransit::stops_as_sf() |>
-    dplyr::group_by(stop_name) |>
-    dplyr::slice_head(n=1) |>
-    dplyr::ungroup() |>
-    dplyr::select(
-      id = stop_id,
-      name = stop_name
-    )  |>
-    sf::st_transform(crs)
   list(
-    routes=routes, 
-    stops=stops
+    routes = gtfs_routes(gtfs, crs),
+    stops = gtfs_stops(gtfs, crs)
   )
 }
 
-gtfs_preprocess <- function(gtfs) {
-  
-}
-
+#' Retrieve Various GTFS Feeds
+#' @name gtfs_feeds
+#' 
+#' @description
+#' `gtfs_mbta()` retrieves the GTFS feed of the Massachusetts Bay Transportation
+#' Authority (MBTA) servicing eastern Massachusetts.
+#' 
+#' `gtfs_mta()` retrieves the GTFS feed of the Metropolitan Transportation
+#' Authority (MTA) servicing the New York City metro area.
+#'
+#' @inheritParams gtfs_helpers
+#'
+#' @returns A list including `routes` and `stops` as `sf` objects.
+#' @export
 gtfs_get_mbta <- function(crs) {
-  gtfs <- tidytransit::read_gtfs("https://cdn.mbta.com/MBTA_GTFS.zip")
-  gtfs$routes <- gtfs$routes |>
-    dplyr::filter(
-      route_desc %in% c("Rapid Transit", "Commuter Rail") |
-        stringr::str_detect(route_short_name, "^SL"))
-  gtfs
+  tidytransit::read_gtfs("https://cdn.mbta.com/MBTA_GTFS.zip") |>
+    gtfs_process(crs)
 }
 
 
 gtfs_get_mta <- function(crs) {
-  gtfs <- tidytransit::read_gtfs("http://web.mta.info/developers/data/nyct/subway/google_transit.zip")
+  tidytransit::read_gtfs(
+    "http://web.mta.info/developers/data/nyct/subway/google_transit.zip"
+    ) |>
+    gtfs_process(crs)
 }
