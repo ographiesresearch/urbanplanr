6be5d16e2fc71b50b81ab919abb591dca3db9cbc
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2025-04-02 15:03:12 -0400

    `munis.R` good to go #17

--- R/munis.R
+++ R/munis.R
@@ -7,18 +7,18 @@
 #' @export
 munis_preprocess <- function(df, crs) {
   df |>
+    st_check_for_proj(crs) |>
     st_geom_to_xy() |>
-    sf_preprocess(crs) |>
     dplyr::select(
-      pl_name,
-      state,
-      x_pl = x,
-      y_pl = y
+      "name",
+      "state",
+      x_pl = "x",
+      y_pl = "y"
     ) |>
     dplyr::mutate(
       pl_id = stringr::str_c(
-        stringr::str_to_lower(pl_name), 
-        stringr::str_to_lower(state),
+        stringr::str_to_lower(.data$name), 
+        stringr::str_to_lower(.data$state),
         sep = "_"
       )
     )
@@ -44,71 +44,77 @@ munis_preprocess <- function(df, crs) {
 #' 
 #' `muni_get_vt()` Downloads municipal boundaries for Vermont.
 #'
+#' @inheritParams munis_preprocess
+#'
 #' @returns An `sf` object.
 #' @export
-munis_get_ma <- function() {
+munis_get_ma <- function(crs) {
   message("Downloading Massachusetts municipal boundaries...")
   utils_get_arc("43664de869ca4b06a322c429473c65e5_0") |>
+    st_preprocess(crs) |>
     dplyr::mutate(
-      town = stringr::str_to_title(town),
+      name = stringr::str_to_title(.data$town),
       state = "MA"
     ) |>
-    dplyr::select(pl_name = town, state)
+    dplyr::select("name", "state")
 }
 
 #' @name muni_get_state
 #' @export
-munis_get_ct <- function() {
+munis_get_ct <- function(crs) {
   message("Downloading Connecticut municipal boundaries...")
   utils_get_arc("df1f6d681b7e41dca8bdd03fc9ae0dd6_1") |>
+    st_preprocess(crs) |>
     dplyr::filter(
-      town != " ", town != ""
+      "town" != " ", "town" != ""
     ) |>
     dplyr::mutate(
       state = "CT"
     ) |>
-    dplyr::group_by(pl_name = town, state) |>
+    dplyr::group_by(name = .data$town, .data$state) |>
     dplyr::summarize(
-      geometry = sf::st_union(geometry)
+      geometry = sf::st_union(.data$geometry)
     ) |>
     dplyr::ungroup()
 }
 
 #' @name muni_get_state
 #' @export
-munis_get_me <- function() {
+munis_get_me <- function(crs) {
   message("Downloading Maine municipal boundaries...")
   utils_get_arc("289a91e826fd4f518debdd824d5dd16d_0") |>
+    st_preprocess(crs) |>
     dplyr::filter(
-      town != " "
+      "town" != " "
     ) |>
     dplyr::mutate(
       state = "ME"
     ) |>
     sf::st_make_valid() |> 
-    dplyr::group_by(pl_name = town, state) |>
+    dplyr::group_by(name = .data$town, .data$state) |>
     dplyr::summarize(
-      geometry = sf::st_union(geometry)
+      geometry = sf::st_union(.data$geometry)
     )
 }
 
 #' @name muni_get_state
 #' @export
-munis_get_nh <- function() {
+munis_get_nh <- function(crs) {
   message("Downloading New Hampshire municipal boundaries...")
   utils_get_arc("4edf75ab263b4d92996f92fb9cf435fa_8") |>
+    st_preprocess(crs) |>
     dplyr::filter(
-      pbpname != " "
+      "name" != " "
     ) |>
     dplyr::mutate(
       state = "NH"
     ) |>
-    dplyr::select(pl_name = pbpname, state)
+    dplyr::select("name", "state")
 }
 
 #' @name muni_get_state
 #' @export
-munis_get_nj <- function() {
+munis_get_nj <- function(crs) {
   url <- httr::parse_url("https://services2.arcgis.com/XVOqAjTOJ5P6ngMu/ArcGIS/rest/services")
   url$path <- paste(url$path, "NJ_Municipal_Boundaries_3424/FeatureServer/0/query", sep = "/")
   url$query <- list(where = "POPDEN2020>0",
@@ -118,60 +124,63 @@ munis_get_nj <- function() {
   request <- httr::build_url(url)
   
   sf::st_read(request) |>
-    dplyr::rename_with(tolower) |>
+    st_preprocess(crs) |>
     dplyr::select(
-      name
+      "name"
     ) |>
     dplyr::mutate(state = "NJ")
 }
 
 #' @name muni_get_state
 #' @export
-munis_get_ny <- function() {
+munis_get_ny <- function(crs) {
   utils_get_remote_shp(
     "https://gisdata.ny.gov/GISData/State/Civil_Boundaries/NYS_Civil_Boundaries.shp.zip",
     layer="Cities_Towns.shp"
   ) |>
+    st_preprocess(crs) |>
     dplyr::select(
-      name
+      "name"
     ) |>
     dplyr::mutate(state = "NY")
 }
 
 #' @name muni_get_state
 #' @export
-munis_get_ri <- function() {
+munis_get_ri <- function(crs) {
   message("Downloading Rhode Island municipal boundaries...")
   utils_get_arc("957468e8bb3245e8b3321a7bf3b6d4aa_0") |>
+    st_preprocess(crs) |>
     dplyr::filter(
-      name != " ", name != ""
+      "name" != " ", "name" != ""
     ) |>
     dplyr::mutate(
-      name = stringr::str_to_title(name),
+      name = stringr::str_to_title(.data$name),
       state = "RI"
     ) |>
-    dplyr::group_by(pl_name = name, state) |>
+    dplyr::group_by(name = .data$name, .data$state) |>
     dplyr::summarize(
-      geometry = sf::st_union(geometry)
+      geometry = sf::st_union(.data$geometry)
     ) |>
     dplyr::ungroup() |>
     dplyr::select(
-      pl_name, state
+      "name", "state"
     )
 }
 
 #' @name muni_get_state
 #' @export
-munis_get_vt <- function() {
+munis_get_vt <- function(crs) {
   message("Downloading Vermont municipal boundaries...")
   utils_get_arc("3f464b0e1980450e9026430a635bff0a_0") |>
+    st_preprocess(crs) |>
     dplyr::filter(
-      townnamemc != " "
+      "townnamemc" != " "
     ) |>
     dplyr::mutate(
       state = "VT"
     ) |>
-    dplyr::select(pl_name = townnamemc, state)
+    dplyr::select(name = "townnamemc", "state")
 }
 
 #' Execute Municipality-Getter Using State Name
