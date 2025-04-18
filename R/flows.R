0c7015b465294bf1b3a11ab197eb31e34dc5f748
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2025-04-18 15:03:12 -0400

    wrap on `flow_terrain()` #26

--- R/flows.R
+++ R/flows.R
@@ -15,33 +15,47 @@ flow_terrain <- function(extent,
                          tiles_on_side = 8,
                          stepped = FALSE
   ) {
+  message("Downloading digital elevation model..")
   dem <- st_get_dem(extent, tiles_on_side = tiles_on_side, expand = expand)
+  message("Done.")
   
+  message("Generating contour lines...")
   contours <- dem |>
     st_contours(interval = interval)
+  message("Done.")
   
+  message("Cropping digital elevation model...")
   dem <- dem |> 
     terra::crop(extent, mask=TRUE)
+  message("Done.")
   
+  message(terra::crs(dem))
+  
+  message("Generating hillshade...")
   hillshade <- dem |>
     st_hillshade(z_scale = z_scale)
+  message("Done.")
   
+  message("Enclosing contours...")
   contours_enc <- contours |>
     st_contours_enclose(extent, poly = TRUE)
-  
+  message("Done.")
+
   stepped_contours <- NULL
   if(stepped) {
-    stepped_contours <- contours_enc |> 
+    message("Creating stepped topography raster...")
+    stepped_contours <- contours_enc |>
       terra::vect() |>
       terra::rasterize(dem, field="z") |>
       st_hillshade(z_scale = 4)
+    message("Done.")
   }
-  
+
   list(
     dem = dem,
     contours = contours,
-    contours_enc = contours_enc,
     hillshade = hillshade,
+    contours_enc = contours_enc,
     stepped = stepped_contours
   )
 }
