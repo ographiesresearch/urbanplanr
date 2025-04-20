bdb2de7a9680fc4464ad635a5cc0adb78e1f391a
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2025-04-20 14:54:39 -0400

    resolve flows

--- R/flows.R
+++ R/flows.R
@@ -16,7 +16,8 @@ flow_terrain <- function(extent,
                          interval = 10,
                          z_scale = 3,
                          tiles_on_side = 8,
-                         stepped = FALSE
+                         stepped = FALSE,
+                         mask = TRUE
   ) {
   message("Downloading digital elevation model..")
   dem <- st_get_dem(extent, tiles_on_side = tiles_on_side, expand = expand)
@@ -29,7 +30,7 @@ flow_terrain <- function(extent,
   
   message("Cropping digital elevation model...")
   dem <- dem |> 
-    terra::crop(extent, mask=mask)
+    terra::crop(extent, snap = "in", mask = mask)
   message("Done.")
   
   message(terra::crs(dem))
@@ -62,3 +63,92 @@ flow_terrain <- function(extent,
     stepped = stepped_contours
   )
 }
+
+# flow_basemap <- function(
+#       states, 
+#       counties,
+#       extent = NULL,
+#       year = NULL,
+#       crs = ifelse(
+#         is.null(extent), 
+#         4326, 
+#         sf::st_crs(extent)
+#         )
+#   ) {
+#   # if (!is.null(extent) & (missing(states) & !missing(counties))) {
+#   #   
+#   # }
+#   list(
+#     states = tigris_get_states(
+#       states = states, 
+#       crs = crs,
+#       year = year
+#     ),
+#     counties = tigris_get_counties(
+#       states = states, 
+#       counties = counties, 
+#       crs = crs,
+#       year = year
+#     ),
+#     places = tigris_get_places(
+#       states = states,
+#       crs = crs,
+#       year = year
+#     ),
+#     block_groups = tigris_get_block_groups(
+#       states = states, 
+#       crs = crs,
+#       year = year
+#     ),
+#     # |>
+#     #   sf::st_join(
+#     #     places |>
+#     #       dplyr::select(placefp), 
+#     #     largest = TRUE
+#     #   ),
+#     census_tracts = tigris_get_tracts(
+#       states = states, 
+#       crs = crs,
+#       year = year
+#     ),
+#     # |>
+#     #   sf::st_join(
+#     #     places |>
+#     #       dplyr::select(.data$placefp), 
+#     #     largest = TRUE
+#     #   ),
+#     area_water = tigris_get_area_water(
+#       states = states, 
+#       counties = counties, 
+#       crs = crs,
+#       year = year
+#     ),
+#     linear_water = tigris_get_linear_water(
+#       states = states, 
+#       counties = counties, 
+#       crs = crs,
+#       year = year
+#     ),
+#     roads = tigris_get_roads(
+#       states = states, 
+#       counties = counties, 
+#       crs = crs,
+#       year = year
+#     ),
+#     rails = tigris_get_rails(
+#       crs = crs,
+#       year = year
+#     )
+#   )
+#   
+#   
+#   
+#   
+#   # message("Downloading places...")
+#   # place_geo <- place_decision(config$states, crs = config$crs)
+#   # 
+#   # if ("places" %in% names(config)) {
+#   #   place_geo <- place_geo |>
+#   #     select_places(places = config$places)
+#   # }
+# }
