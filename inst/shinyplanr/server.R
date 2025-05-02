a7d43c4d9dda6e1bd4cad9f01a49ca2c68529e45
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2025-05-02 18:44:32 -0400

    shiny update

--- inst/shinyplanr/server.R
+++ inst/shinyplanr/server.R
@@ -1,5 +1,9 @@
 server <- function(input, output, session) {
   
+  # Setup ====
+  
+  ## Process Internal Data ====
+  
   counties <- COUNTIES |>
     dplyr::arrange(state_abbrev, county_geoid) |>
     dplyr::mutate(
@@ -22,40 +26,256 @@ server <- function(input, output, session) {
     sf::st_set_geometry("geometry") |>
     st_bbox_sf()
   
+  ## Declare Reactive Values ====
+  
   places <- reactiveVal()
-  places_boundary <- reactiveVal()
+  region <- reactiveVal()
+  crs <- reactiveValues()
+  crs$units <- "ft"
+  
+  ## Initialize Map ====
+  
+  output$map <- mapgl::renderMaplibre({
+    mapgl::maplibre(
+      bounds = init_bbox,
+      style = mapgl::carto_style(
+        if(input$darklight == "dark") "dark-matter" else "positron"
+      )
+    )
+  })
+  
+  # Dynamic UI ====
+  
+  ## Place Selections ====
+
+  output$place_selector <- renderUI({
+    if (input$mode %in% c("states", "counties", "places")) {
+      list(
+        shiny::selectizeInput(
+          inputId = 'selected_places',
+          label = glue::glue("{stringr::str_to_sentence(input$mod)}"),
+          choices = NULL,
+          multiple = TRUE,
+          options = list(dropdownParent = 'body')
+        )
+      )
+    } else if (input$mode == "file") {
+      fileInput("file", "Choose a File")
+    } else if (input$mode == "map") {
+      
+    }
+  })
+  
+  ## Populate Place Selections ====
   
   shiny::observeEvent(
-    input$placemode, {
+    input$mode, {
       places(NULL)
-      places_boundary(NULL)
-      if (input$placemode == "counties") {
-        shiny::updateSelectizeInput(
-          session,
-          "place_select",
-          choices = counties$long,
-          server = TRUE
-        )
-      } else if (input$placemode == "states") {
-        shiny::updateSelectizeInput(
-          session,
-          "place_select",
-          choices = states$state_abbrev,
-          server = TRUE
-        )
-      } else if (input$placemode == "places") {
-        shiny::updateSelectizeInput(
-          session,
-          "place_select",
-          choices = places_list$long,
-          server = TRUE
-        )
+      region(NULL)
+      if (input$mode == "counties") {
+        choices <- counties$long
+      } else if (input$mode == "states") {
+        choices <- states$state_abbrev
+      } else if (input$mode == "places") {
+        choices <- places_list$long
+      } else {
+        choices <- NULL
       }
+      shiny::updateSelectizeInput(
+        session,
+        "selected_places",
+        choices = choices,
+        server = TRUE
+      )
     }
   )
   
-  observeEvent(input$placemode, ignoreNULL = FALSE, {
-    if (input$placemode == "map") {
+  shiny::observeEvent(crs$suggest, {
+    print("hello2")
+    shiny::updateSelectizeInput(
+      session,
+      "choose_crs",
+      choices = crs$suggest$crs_name
+    )
+  })
+  
+  observeEvent(input$button_confirm_crs, {
+    shiny::req(crs$suggest)
+    print(crs$suggest)
+    crs <- crs$suggest |>
+      dplyr::filter(crs_name == input$choose_crs) |>
+      dplyr::pull(crs_code) |>
+      as.numeric()
+    places() |>
+      sf::st_transform(crs) |>
+      places()
+    bslib::accordion_panel_open(
+      "panels",
+      "Region"
+    )
+  })
+  
+  ## Build Table Settings ====
+  
+  # output$table_settings <- renderUI({
+  #   if (!is.null(places())) {
+  #     bslib::accordion_panel(
+  #       title = "Table",
+  #         shiny::fluidRow(
+  #           shiny::column(
+  #             width = 6,
+  #             shiny::selectizeInput(
+  #               "place_id_col",
+  #               "ID Column",
+  #               choices = names(places()),
+  #               selected = if ("id" %in% names(places())) {"id"} else {NULL},
+  #               width = "100%",
+  #               options = list(dropdownParent = 'body')
+  #             )
+  #           ),
+  #           shiny::column(
+  #             width = 6,
+  #             shiny::selectizeInput(
+  #               "place_name_col",
+  #               "Name Column",
+  #               choices = names(places()),
+  #               selected = if ("name" %in% names(places())) { "name" } else {NULL},
+  #               width = "100%",
+  #               options = list(dropdownParent = 'body')
+  #             )
+  #           )
+  #         )
+  #     )
+  #   }
+  # })
+  
+  # Data Processing ====
+  
+  ## Update Buffer Based on Regionalizer ====
+  
+  shiny::observeEvent(input$button_update_region, {
+    d <- places() |>
+      sf::st_buffer(units::as_units(
+        input$region,
+        input$region_units
+      )) |>
+      sf::st_union() |>
+      sf::st_as_sf(crs = sf::st_crs(places())) |>
+      sf::st_set_geometry("geometry")
+    
+    if (input$region_type == "bbox") {
+      d <- d |>
+        st_bbox_sf()
+    } else if (input$region_type == "square") {
+      d <- d |>
+        st_square_it()
+    } else if (input$region_type == "circle") {
+      d <- d |>
+        st_square_it(circle = TRUE)
+    }
+    d |>
+      region()
+  })
+  
+  # I/O ====
+  
+  ## input$file listener. ====
+  
+  observeEvent(input$file, {
+    sf::st_read(input$file$datapath) |>
+      places()
+  })
+  
+  ## input$button_confirm_places() listener. ====
+  
+  observeEvent(input$button_confirm_places, {
+    shiny::req(input$selected_places)
+    withProgress(
+      message = glue::glue("Fetching {input$select_mode}!"),
+      detail = "Should only be a few seconds...", {
+      if (input$mode == "counties") {
+        select <- counties |> 
+          dplyr::filter(long %in% input$selected_places) |>
+          dplyr::mutate(
+            long = stringr::str_replace(long, ",", " COUNTY,")
+          ) |>
+          dplyr::pull(long) |>
+          tigris_get_counties()
+      } else if (input$mode == "states") {
+        select <- states |> 
+          dplyr::filter(state_abbrev %in% input$selected_places) |>
+          dplyr::pull(state_abbrev) |>
+          tigris_get_states()
+      } else if (input$mode == "places") {
+        select <- places_list |> 
+          dplyr::filter(long %in% input$selected_places) |>
+          dplyr::pull(long) |>
+          munis_get_munis()
+      } else if (input$mode == "map") {
+        # Drawn Features NOT WORKING
+        select <- mapgl::maplibre_proxy("map") |>
+          mapgl::get_drawn_features()
+      }
+      places(select)
+    })
+    bslib::accordion_panel_open(
+      "panels",
+      "CRS"
+    )
+  })
+  
+  # Map ==== 
+  
+  mapgl_add_remove_fill <- function(map, layername, layer, animate = FALSE) {
+    map |>
+      mapgl::clear_layer(layername)
+    
+    shiny::req(!is.null(layer))
+    
+    map |>
+      mapgl::add_fill_layer(
+        id = layername,
+        source = layer,
+        fill_color = "#990000",
+        fill_opacity = 0.3,
+        fill_outline_color = "white"
+      ) |>
+      mapgl::fit_bounds(layer, animate = TRUE)
+  }
+  
+  ## places() listener. ====
+  
+  observeEvent(places(), ignoreNULL = FALSE, {
+    mapgl::maplibre_proxy("map") |>
+      mapgl_add_remove_fill("places", places())
+    if (is.null(places())) {
+      crs$suggest <- NULL
+    } else {
+      print("hello")
+      crs$suggest <- crsuggest::suggest_crs(places(), gcs = 4269, limit = 5, units = crs$units)
+    }
+  })
+  
+  ## region() listener. ====
+  
+  observeEvent(region(), ignoreNULL = FALSE, {
+    mapgl::maplibre_proxy("map") |>
+      mapgl_add_remove_fill("region", region())
+  })
+  
+  ## `darklight` listener. ====
+  
+  shiny::observeEvent(input$darklight, {
+    mapgl::maplibre_proxy("map") |>
+      mapgl::set_style(
+        mapgl::carto_style(if(input$darklight == "dark") "dark-matter" else "positron")
+      )
+  })
+  
+  ## input$mode light/dark listener. ====
+  
+  observeEvent(input$mode, ignoreNULL = FALSE, {
+    if (input$mode == "map") {
       mapgl::maplibre_proxy("map") |>
         mapgl::add_draw_control(
           controls = c(
@@ -68,210 +288,5 @@ server <- function(input, output, session) {
       mapgl::maplibre_proxy("map") |>
         mapgl::clear_controls()
     }
-  })
-
-  output$place_select <- renderUI({
-    if (input$placemode %in% c("states", "counties", "places")) {
-      list(
-        shiny::selectizeInput(
-          inputId = 'place_select',
-          label = "Places",
-          choices = NULL,
-          multiple = TRUE,
-          options = list(dropdownParent = 'body')
-        ),
-        shiny::actionButton(
-          "button_get_places", 
-          "Fetch Selections",
-          width = "100%"
-        )
-      )
-    } else if (input$placemode == "file") {
-      fileInput("file", "Choose a File")
-    } else if (input$placemode == "map") {
-      shiny::actionButton(
-        "button_get_drawn", 
-        "Finish Drawing",
-        width = "100%"
-      )
-    }
-  })
-  
-  output$place_settings <- renderUI({
-    if (!is.null(places())) {
-      list(
-        bslib::card(
-          h3("Boundary Settings"),
-          shiny::fluidRow(
-            shiny::column(
-              width = 6,
-              numericInput( 
-                "place_buffer", 
-                "Buffer", 
-                value = 0, 
-                min = 1,
-                width = "100%"
-              )
-            ),
-            shiny::column(
-              width = 6,
-              selectizeInput( 
-                "place_buffer_units", 
-                "Units", 
-                list(
-                  "Miles" = "miles", 
-                  "Kilometers" = "km",
-                  "Feet" = "feet",
-                  "Meters" = "meters"
-                ),
-                width = "100%"
-              )
-            )
-          ),
-          shiny::fluidRow(
-            shiny::column(
-              width = 6,
-              checkboxInput("place_buffer_as_bbox", "Boundary as Bounding Box?", FALSE)
-            ),
-            shiny::column(
-              width = 6,
-              shiny::actionButton(
-                "update_place_buffer", 
-                "Update",
-                width = "100%"
-              )
-            )
-          )
-        ),
-        bslib::card(
-          h3("Place Settings"),
-          shiny::fluidRow(
-            shiny::column(
-              width = 6,
-              shiny::selectizeInput(
-                "place_id_col", 
-                "ID Column", 
-                choices = names(places()),
-                selected = if ("id" %in% names(places())) {"id"} else {NULL},
-                width = "100%",
-                options = list(dropdownParent = 'body')
-              )
-            ),
-            shiny::column(
-              width = 6,
-              shiny::selectizeInput(
-                "place_name_col",
-                "Name Column",
-                choices = names(places()),
-                selected = if ("name" %in% names(places())) { "name" } else {NULL},
-                width = "100%",
-                options = list(dropdownParent = 'body')
-              )
-            )
-          )
-        )
-      )
-    }
-  })
-  
-  shiny::observeEvent(input$update_place_buffer, {
-    d <- places() |>
-      sf::st_buffer(units::as_units(
-        input$place_buffer,
-        input$place_buffer_units
-      )) |>
-      sf::st_union() |>
-      sf::st_as_sf() |>
-      sf::st_set_geometry("geometry")
-    
-    if (input$place_buffer_as_bbox) {
-      d <- d |>
-        st_bbox_sf()
-    }
-    d |>
-      places_boundary()
-  })
-  
-  output$map <- mapgl::renderMaplibre({
-    mapgl::maplibre(
-      bounds = init_bbox
-      )
-  })
-  
-  observeEvent(input$file, {
-    sf::st_read(input$file$datapath) |>
-      places()
-  })
-  
-  observeEvent(places(), ignoreNULL = FALSE, {
-    mapgl::maplibre_proxy("map") |>
-      mapgl::clear_layer("places")
-    
-    shiny::req(!is.null(places()))
-    
-    mapgl::maplibre_proxy("map") |>
-      mapgl::add_line_layer(
-        id = "places",
-        source = places(),
-        line_color = "blue"
-      ) |>
-      mapgl::fit_bounds(places(), animate = FALSE)
-    }
-  )
-  
-  observeEvent(places_boundary(), ignoreNULL = FALSE, {
-    mapgl::maplibre_proxy("map") |>
-      mapgl::clear_layer("placesBoundary")
-    
-    shiny::req(!is.null(places_boundary()))
-    
-    mapgl::maplibre_proxy("map") |>
-      mapgl::add_line_layer(
-        id = "placesBoundary",
-        source = places_boundary(),
-        line_color = "red"
-      ) |>
-      mapgl::fit_bounds(places_boundary(), animate = TRUE)
-  })
-  
-  observeEvent(input$button_get_drawn, {
-    withProgress(
-      message = glue::glue("Fetching {input$placemode}!"),
-      detail = "Should only be a few seconds...",
-      features <- mapgl::maplibre_proxy("map") |>
-        mapgl::get_drawn_features()
-      )
-    output$drawn_features <- renderPrint({
-      print(features)
-    })
-  })
-  
-  observeEvent(input$button_get_places, {
-    withProgress(
-      message = glue::glue("Fetching {input$placemode}!"),
-      detail = "Should only be a few seconds...",
-      if (input$placemode == "counties") {
-          select <- counties |> 
-            dplyr::filter(long %in% input$place_select) |>
-            dplyr::mutate(
-              long = stringr::str_replace(long, ",", " COUNTY,")
-            ) |>
-            dplyr::pull(long) |>
-            tigris_get_counties() |>
-            places()
-      } else if (input$placemode == "states") {
-        select <- states |> 
-          dplyr::filter(state_abbrev %in% input$place_select) |>
-          dplyr::pull(state_abbrev) |>
-          tigris_get_states() |>
-          places()
-      } else if (input$placemode == "places") {
-        select <- places_list |> 
-          dplyr::filter(long %in% input$place_select) |>
-          dplyr::pull(long) |>
-          munis_get_munis() |>
-          places()
-      }
-  )
   })
 }
--- inst/shinyplanr/ui.R
+++ inst/shinyplanr/ui.R
@@ -1,27 +1,144 @@
 ui <- bslib::page_sidebar(
-  # tags$head(
-  #   tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
-  # ),
   sidebar = bslib::sidebar(
     width = "40%",
-    bslib::card(
-      shiny::selectizeInput( 
-        "placemode",
-        "How would you like to specify your study area?",
-        list(
-          "Upload a File" = "file",
-          "Select Places" = "places",
-          "Select Counties" = "counties",
-          "Select States" = "states",
-          "Draw on the Map" = "map"
+    open = "always",
+    bslib::input_dark_mode(id="darklight"),
+    shiny::h1(shiny::code("urbanplanr")),
+    bslib::accordion(
+      multiple = FALSE,
+      id = "panels",
+      bslib::accordion_panel(
+        title = "Selection",
+        shiny::selectizeInput( 
+          "mode",
+          "How would you like to specify your study area?",
+          list(
+            "Upload a File" = "file",
+            "Select Places" = "places",
+            "Select Counties" = "counties",
+            "Select States" = "states"
+          ),
+          options = list(dropdownParent = 'body'),
+          multiple = FALSE,
         ),
-        options = list(dropdownParent = 'body'),
-        multiple = FALSE
+        shiny::uiOutput("place_selector"),
+        shiny::actionButton(
+          "button_confirm_places", 
+          "Continue",
+          width = "100%"
+        )
       ),
-      shiny::uiOutput("place_select"),
-    ),
-    shiny::uiOutput("place_settings"),
-    shiny::verbatimTextOutput("drawn_features")
+      shiny::uiOutput("table_settings"),
+      bslib::accordion_panel(
+        title = "CRS",
+        shiny::fluidRow(
+          shiny::column(
+            width = 12,
+            shiny::selectizeInput(
+              "choose_crs",
+              "Choose a Coordinate Reference System",
+              choices = NULL,
+              selected = NULL,
+              width = "100%",
+              options = list(dropdownParent = 'body')
+            )
+          )
+        ),
+        shiny::fluidRow(
+          shiny::column(
+            width = 12,
+            shiny::actionButton(
+              "button_confirm_crs",
+              "Continue",
+              width = "100%"
+            )
+          )
+        )
+      ),
+      bslib::accordion_panel(
+        title = "Region",
+        shiny::fluidRow(
+          shiny::column(
+            width = 6,
+            numericInput(
+              "region",
+              "Buffer",
+              value = 0,
+              min = 1,
+              width = "100%"
+            )
+          ),
+          shiny::column(
+            width = 6,
+            selectizeInput(
+              "region_units",
+              "Units",
+              list(
+                "Miles" = "miles",
+                "Kilometers" = "km",
+                "Feet" = "feet",
+                "Meters" = "meters"
+              ),
+              width = "100%"
+            )
+          )
+        ),
+        
+        shiny::fluidRow(
+          shiny::column(
+            width = 6,
+            shiny::fluidRow(
+              selectizeInput(
+                "region_type",
+                "Type",
+                list(
+                  "Square" = "square",
+                  "Circle" ="circle",
+                  "Bounding Box" = "bbox",
+                  "Radial Buffer" = "buffer"
+                )
+              )
+            )
+          ),
+          shiny::column(
+            width = 6,
+            shiny::actionButton(
+              "button_update_region",
+              "Update",
+              width = "100%"
+            )
+          )
+        ),
+        shiny::actionButton(
+          "button_confirm_region",
+          "Continue",
+          width = "100%"
+        )
+      )
+      # bslib::accordion_panel(
+      #   title = "Output",
+      #   shiny::fluidRow(
+      #     shiny::column(
+      #       width = 6,
+      #       textInput( 
+      #         "outname", 
+      #         "File/DB Name",
+      #         value = "results"
+      #       )
+      #     ),
+      #     shiny::column(
+      #       width = 6,
+      #       shiny::selectizeInput(
+      #         inputId = 'out_format',
+      #         label = "Format",
+      #         choices = c("Geopackage" = "gpkg", "PostGIS" = "postgis"),
+      #         multiple = FALSE,
+      #         options = list(dropdownParent = 'body')
+      #       )
+      #     )
+      #   )
+      # )
+    )
   ),
   mapgl::maplibreOutput("map")
 )
