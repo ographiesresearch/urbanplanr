bdb6e590038002620e7d738506eda857dd21a9fc
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2025-05-08 15:30:23 -0400

    un-functionity `processinternal.R`

--- data-raw/processinternal.R
+++ data-raw/processinternal.R
@@ -1,31 +1,3 @@
-states <- function() {
-  tigris::states() |>
-    st_bbox_sf() |>
-    dplyr::select(
-      geoid=GEOID,
-      state_abbrev=STUSPS,
-      state_name=NAME
-    )
-}
-
-counties <- function(states) {
-  counties <- tigris::counties() |>
-    st_bbox_sf() |>
-    dplyr::select(
-      county_name = NAME,
-      state_geoid = STATEFP,
-      county_id = COUNTYFP,
-      county_geoid = GEOID
-    ) |>
-    dplyr::left_join(
-      states |> sf::st_drop_geometry(),
-      by = dplyr::join_by("state_geoid"=="geoid")
-    ) |>
-    dplyr::mutate(
-      long = stringr::str_to_lower(stringr::str_c(county_name, state_abbrev, sep=","))
-    )
-}
-
 UTM_ZONES <- file.path("data-raw", "utm_zones.geojson") |>
   sf::st_read() |>
   dplyr::select(zone_num) |>
@@ -37,8 +9,28 @@ MUNIS <- state.abb[! state.abb %in% c("SD")] |>
   munis_get_munis(4326) |>
   st_bbox_sf()
 
-STATES <- states()
+STATES <- tigris::states() |>
+  st_bbox_sf() |>
+  dplyr::select(
+    geoid=GEOID,
+    state_abbrev=STUSPS,
+    state_name=NAME
+  )
 
-COUNTIES <- counties(STATES)
+COUNTIES <- tigris::counties() |>
+  st_bbox_sf() |>
+  dplyr::select(
+    county_name = NAME,
+    state_geoid = STATEFP,
+    county_id = COUNTYFP,
+    county_geoid = GEOID
+  ) |>
+  dplyr::left_join(
+    STATES |> sf::st_drop_geometry(),
+    by = dplyr::join_by("state_geoid"=="geoid")
+  ) |>
+  dplyr::mutate(
+    long = stringr::str_to_lower(stringr::str_c(county_name, state_abbrev, sep=","))
+  )
 
 usethis::use_data(COUNTIES, STATES, UTM_ZONES, MUNIS, internal=TRUE, overwrite = TRUE)
